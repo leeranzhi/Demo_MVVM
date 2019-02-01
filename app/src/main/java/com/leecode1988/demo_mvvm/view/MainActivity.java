22acc1c071479306e842a63ac797e9f5a0791bff
leecode <2804581329@qq.com>
2019-02-01 23:21:07 +0800

    删除多余部分。

--- app/src/main/java/com/leecode1988/demo_mvvm/view/MainActivity.java
+++ app/src/main/java/com/leecode1988/demo_mvvm/view/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         ActivityMainBinding mainBinding = DataBindingUtil.
                 setContentView(this, R.layout.activity_main);
 
